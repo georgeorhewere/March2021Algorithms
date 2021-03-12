fef98be4fdbcee0b6274308b754b7532bb1c3aae
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-03-12 11:31:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace March2021Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alogorithm Practice March 2021");
+            testValidString();
+
+        }
+
+        static void testValidString()
+        {
+            ValidStringManager manager = new ValidStringManager();
+            string testInput = "abc";
+            manager.checkStringIsValid(testInput);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ValidStringManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace March2021Algorithms
+{
+    public class ValidStringManager
+    {
+
+
+
+        public void checkStringIsValid(string input)
+        {
+            var charGrps = input.GroupBy(x => x).OrderBy(d => d.Count()).ToList();
+            charGrps.ForEach(d => Console.WriteLine($"{d.Key }  Count : {d.Count()}"));
+
+        }
+
+    }
+}
